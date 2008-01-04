d2bf43e4b1d197accb8c396fbf7d89126affdbad
Roger Binns <rogerb@rogerbinns.com>
2008-01-04 11:12:42 +0000

    Make it possible to run valgrind on other files

--- valgrind.sh
+++ valgrind.sh
@@ -20,5 +20,10 @@ INCLUDEDIR=`$PYTHON -c "import distutils.sysconfig; print distutils.sysconfig.ge
 set -x
 gcc -pthread -fno-strict-aliasing  -g -fPIC -W -Wall -DEXPERIMENTAL -DSQLITE_THREADSAFE=1 -DAPSW_USE_SQLITE_AMALGAMATION=\"sqlite3/sqlite3.c\" -I$INCLUDEDIR -c apsw.c
 gcc -pthread  -g -shared apsw.o -o apsw.so
+if [ $# -eq 0 ]
+then
 env APSW_NO_MEMLEAK=t APSW_TEST_ITERATIONS=150 valgrind --num-callers=50 --leak-check=full --show-reachable=yes --freelist-vol=50000000 $PYTHON tests.py
+else
+valgrind --num-callers=50 --leak-check=full --show-reachable=yes --freelist-vol=50000000 $PYTHON "$@"
+fi
 
