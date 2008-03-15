7017aa4380155f5e41717851b33724e5070dc385
Roger Binns <rogerb@rogerbinns.com>
2008-03-15 10:45:23 +0000

    Pick amalgamation out of the current directory

--- coverage.sh
+++ coverage.sh
@@ -16,7 +16,7 @@ rm -f *.gcda *.gcov *.gcno apsw.so
 PYTHON=python # use whatever is in the path
 INCLUDEDIR=`$PYTHON -c "import distutils.sysconfig; print distutils.sysconfig.get_python_inc()"`
 set -x
-gcc -pthread -fno-strict-aliasing -ftest-coverage -fprofile-arcs -g -fPIC -Wall -Wextra -DEXPERIMENTAL -DSQLITE_THREADSAFE=1 -DAPSW_USE_SQLITE_AMALGAMATION=\"sqlite3/sqlite3.c\" -I$INCLUDEDIR -c apsw.c
+gcc -pthread -fno-strict-aliasing -ftest-coverage -fprofile-arcs -g -fPIC -Wall -Wextra -DEXPERIMENTAL -DSQLITE_THREADSAFE=1 -DAPSW_USE_SQLITE_AMALGAMATION=\"sqlite3.c\" -I$INCLUDEDIR -c apsw.c
 gcc -pthread -ftest-coverage -fprofile-arcs -g -shared apsw.o -o apsw.so
 gcc -fPIC -shared -o testextension.sqlext -Isqlite3 testextension.c
 $PYTHON $args
--- valgrind.sh
+++ valgrind.sh
@@ -5,10 +5,11 @@
 # You should build a debug Python, with this being an example using /space/pydebug as
 # the root.
 #
+# ver=2.5.2
 # mkdir /space/pydebug
 # cd /space/pydebug
-# wget -O -  http://www.python.org/ftp/python/2.5.1/Python-2.5.1.tar.bz2 | tar xfj -
-# cd Python-2.5.1
+# wget -O -  http://www.python.org/ftp/python/$ver/Python-$ver.tar.bz2 | tar xfj -
+# cd Python-$ver
 # ./configure --with-pydebug --without-pymalloc --prefix=/space/pydebug
 # make install
 #
@@ -18,12 +19,12 @@
 PYTHON=python # use whatever is in the path
 INCLUDEDIR=`$PYTHON -c "import distutils.sysconfig; print distutils.sysconfig.get_python_inc()"`
 set -x
-gcc -pthread -fno-strict-aliasing  -g -fPIC -W -Wall -DEXPERIMENTAL -DSQLITE_THREADSAFE=1 -DAPSW_USE_SQLITE_AMALGAMATION=\"sqlite3/sqlite3.c\" -I$INCLUDEDIR -c apsw.c
+gcc -pthread -fno-strict-aliasing  -g -fPIC -W -Wall -DEXPERIMENTAL -DSQLITE_THREADSAFE=1 -DAPSW_USE_SQLITE_AMALGAMATION=\"sqlite3.c\" -I$INCLUDEDIR -c apsw.c
 gcc -pthread  -g -shared apsw.o -o apsw.so
 if [ $# -eq 0 ]
 then
-env APSW_NO_MEMLEAK=t APSW_TEST_ITERATIONS=150 valgrind --num-callers=50 --leak-check=full --show-reachable=yes --freelist-vol=50000000 $PYTHON tests.py
+    env APSW_NO_MEMLEAK=t APSW_TEST_ITERATIONS=150 valgrind --num-callers=50 --leak-check=full --show-reachable=yes --freelist-vol=50000000 $PYTHON tests.py
 else
-valgrind --num-callers=50 --leak-check=full --show-reachable=yes --freelist-vol=50000000 $PYTHON "$@"
+    valgrind --num-callers=50 --leak-check=full --show-reachable=yes --freelist-vol=50000000 $PYTHON "$@"
 fi
 
