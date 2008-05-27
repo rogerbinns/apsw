68cd6478b19e46b92cd064273b04eeb670e37e32
Roger Binns <rogerb@rogerbinns.com>
2008-05-27 03:41:09 +0000

    Let environment variable override iteration count

--- valgrind.sh
+++ valgrind.sh
@@ -18,12 +18,13 @@
 # find python
 PYTHON=python # use whatever is in the path
 INCLUDEDIR=`$PYTHON -c "import distutils.sysconfig; print distutils.sysconfig.get_python_inc()"`
+APSW_TEST_ITERATIONS=${APSW_TEST_ITERATIONS:=150}
 set -x
 gcc -pthread -fno-strict-aliasing  -g -fPIC -W -Wall -DAPSW_NO_NDEBUG -DEXPERIMENTAL -DSQLITE_THREADSAFE=1 -DAPSW_USE_SQLITE_AMALGAMATION=\"sqlite3.c\" -I$INCLUDEDIR -c apsw.c
 gcc -pthread  -g -shared apsw.o -o apsw.so
 if [ $# -eq 0 ]
 then
-    env APSW_NO_MEMLEAK=t APSW_TEST_ITERATIONS=150 valgrind --num-callers=50 --leak-check=full --show-reachable=yes --freelist-vol=50000000 $PYTHON tests.py
+    env APSW_NO_MEMLEAK=t APSW_TEST_ITERATIONS=$APSW_TEST_ITERATIONS valgrind --num-callers=50 --leak-check=full --show-reachable=yes --freelist-vol=50000000 $PYTHON tests.py
 else
     valgrind --num-callers=50 --leak-check=full --show-reachable=yes --freelist-vol=50000000 $PYTHON "$@"
 fi
