823d131e5af940100583e4694167a3f1621de96b
Roger Binns <rogerb@rogerbinns.com>
2008-06-02 00:19:44 +0000

    Ensure -DNDEBUG

--- setup.py
+++ setup.py
@@ -8,6 +8,9 @@ define_macros=[]
 # We always want threadsafe
 define_macros.append( ('SQLITE_THREADSAFE', '1') )
 
+# We don't want assertions
+define_macros.append( ('NDEBUG', '1') )
+
 # This includes the functionality marked as experimental in SQLite 3.
 # Comment out the line to exclude them
 define_macros.append( ('EXPERIMENTAL', '1') )
