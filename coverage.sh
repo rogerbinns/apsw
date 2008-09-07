4473a46d86485135671686ad76493686ec50f334
Roger Binns <rogerb@rogerbinns.com>
2008-09-07 10:38:14 +0000

    * Add -D_FORTIFY_SOURCE=2 to get even more warnings (also requires -O2) * Increase leak resolution to really find all memory in use at end

--- coverage.sh
+++ coverage.sh
@@ -10,13 +10,15 @@ else
   args="$@"
 fi
 
+OPT=${OPT:=}
+
 # Measure code coverage
 rm -f *.gcda *.gcov *.gcno apsw.so
 # find python
 PYTHON=python # use whatever is in the path
 INCLUDEDIR=`$PYTHON -c "import distutils.sysconfig,sys; sys.stdout.write(distutils.sysconfig.get_python_inc())"`
 set -x
-gcc -pthread -fno-strict-aliasing -ftest-coverage -fprofile-arcs -g -fPIC -Wall -Wextra -DEXPERIMENTAL -DSQLITE_THREADSAFE=1 -DAPSW_USE_SQLITE_AMALGAMATION=\"sqlite3.c\" -DAPSW_NO_NDEBUG -DAPSW_TESTFIXTURES -I$INCLUDEDIR -c apsw.c
+gcc -pthread -fno-strict-aliasing -ftest-coverage -fprofile-arcs -g $(OPT) -fPIC -Wall -Wextra -D_FORTIFY_SOURCE=2 -DEXPERIMENTAL -DSQLITE_THREADSAFE=1 -DAPSW_USE_SQLITE_AMALGAMATION=\"sqlite3.c\" -DAPSW_NO_NDEBUG -DAPSW_TESTFIXTURES -I$INCLUDEDIR -c apsw.c
 gcc -pthread -ftest-coverage -fprofile-arcs -g -shared apsw.o -o apsw.so
 gcc -fPIC -shared -Isqlite3 -I. -o testextension.sqlext -Isqlite3 testextension.c
 set +e
