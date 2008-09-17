29c87713cb5c0487ca6e5a3bf425d294c4a51baa
Roger Binns <rogerb@rogerbinns.com>
2008-09-17 10:41:37 +0000

    Turn on FORTIFY_SOURCE, increase leak resolution, more comments on how to get python to free more things on shutdown

--- valgrind.sh
+++ valgrind.sh
@@ -10,22 +10,27 @@
 # cd /space/pydebug
 # wget -O -  http://www.python.org/ftp/python/$ver/Python-$ver.tar.bz2 | tar xfj -
 # cd Python-$ver
-# ./configure --with-pydebug --without-pymalloc --prefix=/space/pydebug
+# # MAXSAVESIZE prevents caching of unused tuples which show up as still reachable at exit
+# # __INSURE__ forces freeing of interned strings at exit
+# # MAXFREEDICTS prevents caching of dictionaries - you will need to edit Objects/dictobject.c to allow
+# #              this override
+# ./configure --with-pydebug --without-pymalloc --prefix=/space/pydebug CPPFLAGS="-DMAXSAVESIZE=0 -D__INSURE__ -DMAXFREEDICTS=0"
 # make install
 #
-# Then put /space/pydebug/bin/ first on your path
+# Then put /space/pydebug/bin/ first on your path.  The CPPFLAGS setting is to make sure no tuples are saved on freelists
 
 # find python
 PYTHON=python # use whatever is in the path
 INCLUDEDIR=`$PYTHON -c "import distutils.sysconfig, sys; sys.stdout.write(distutils.sysconfig.get_python_inc())"`
 APSW_TEST_ITERATIONS=${APSW_TEST_ITERATIONS:=150}
 set -x
-gcc -pthread -fno-strict-aliasing  -g -fPIC -W -Wall -DAPSW_TESTFIXTURES -DAPSW_NO_NDEBUG -DEXPERIMENTAL -DSQLITE_THREADSAFE=1 -DAPSW_USE_SQLITE_AMALGAMATION=\"sqlite3.c\" -I$INCLUDEDIR -c apsw.c
+rm -f apsw.o apsw.so
+gcc -pthread -fno-strict-aliasing  -Os -g -D_FORTIFY_SOURCE=2 -fPIC -W -Wall -DAPSW_TESTFIXTURES -DAPSW_NO_NDEBUG -DEXPERIMENTAL -DSQLITE_THREADSAFE=1 -DAPSW_USE_SQLITE_AMALGAMATION=\"sqlite3.c\" -I$INCLUDEDIR -c apsw.c
 gcc -pthread  -g -shared apsw.o -o apsw.so
 if [ $# -eq 0 ]
 then
-    env APSW_NO_MEMLEAK=t APSW_TEST_ITERATIONS=$APSW_TEST_ITERATIONS valgrind --num-callers=50 --leak-check=full --show-reachable=yes --freelist-vol=50000000 $PYTHON tests.py
+    env APSW_NO_MEMLEAK=t APSW_TEST_ITERATIONS=$APSW_TEST_ITERATIONS valgrind --num-callers=100 --leak-check=full --leak-resolution=high --show-reachable=yes --freelist-vol=500000000 $PYTHON tests.py 
 else
-    valgrind --num-callers=50 --leak-check=full --show-reachable=yes --freelist-vol=50000000 $PYTHON "$@"
+    env APSW_NO_MEMLEAK=t APSW_TEST_ITERATIONS=$APSW_TEST_ITERATIONS valgrind --num-callers=100 --leak-check=full --leak-resolution=high --show-reachable=yes --freelist-vol=500000000 $PYTHON "$@"
 fi
 
