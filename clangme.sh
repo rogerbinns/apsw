9770f5d67cca63a836aad05a58fae5ba14b7d7c2
Roger Binns <rogerb@rogerbinns.com>
2008-09-27 08:48:11 +0000

    Enable checking of icu/rtree/fts code as well

--- clangme.sh
+++ clangme.sh
@@ -39,4 +39,4 @@ do
   sed -i -e "/#include \"${i}.c\"/{r ${i}.c" -e 'd;}' p-apsw.c
 done
 
-$CLANGDIR/tools/clang/utils/scan-build gcc -DSQLITE_DEBUG -DAPSW_TESTFIXTURES -DAPSW_NO_NDEBUG -DEXPERIMENTAL -DSQLITE_THREADSAFE=1 -DAPSW_USE_SQLITE_AMALGAMATION=\"sqlite3.c\" -I. -I/usr/include/python2.5 -c $args
+$CLANGDIR/tools/clang/utils/scan-build gcc -DSQLITE_ENABLE_FTS -DSQLITE_ENABLE_RTREE -DSLITE_ENABLE_ICU -DSQLITE_DEBUG -DAPSW_TESTFIXTURES -DAPSW_NO_NDEBUG -DEXPERIMENTAL -DSQLITE_THREADSAFE=1 -DAPSW_USE_SQLITE_AMALGAMATION=\"sqlite3.c\" -I. -I/usr/include/python2.5 -c $args
