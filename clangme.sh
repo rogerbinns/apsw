b39edd1768ab108ce45e089da774cbe22745c2ca
Roger Binns <rogerb@rogerbinns.com>
2008-10-12 07:04:31 +0000

    Thanks to patch in http://llvm.org/bugs/show_bug.cgi?id=2828 analyser now looks at #included .c files

--- clangme.sh
+++ clangme.sh
@@ -6,7 +6,7 @@ CLANGDIR=/space/llvm
 
 if [ $# = 0 ]
 then
-  args="p-apsw.c"
+  args="apsw.c"
 else
   args="$@"
 fi
@@ -23,20 +23,5 @@ export PATH=$CLANGDIR/Release/bin:$CLANGDIR/bin:$PATH
     cd ../..
     make -j4
 )
-
-# clang doesn't analyse files that are #included so we have to generate an output file that has already done the inclusions
-
-rm -f p-apsw.c
-
-cp apsw.c p-apsw.c
-
-# ::TODO:: work out if I can insert #line directives and if clang pays any attention
-
-sed -i -e '/#include APSW_USE_SQLITE_AMALGAMATION/{r sqlite3.c' -e 'd;}' p-apsw.c 
-
-for i in traceback pointerlist statementcache
-do
-  sed -i -e "/#include \"${i}.c\"/{r ${i}.c" -e 'd;}' p-apsw.c
-done
 
 $CLANGDIR/tools/clang/utils/scan-build gcc -DSQLITE_ENABLE_FTS -DSQLITE_ENABLE_RTREE -DSLITE_ENABLE_ICU -DSQLITE_DEBUG -DAPSW_TESTFIXTURES -DAPSW_NO_NDEBUG -DEXPERIMENTAL -DSQLITE_THREADSAFE=1 -DAPSW_USE_SQLITE_AMALGAMATION=\"sqlite3.c\" -I. -I/usr/include/python2.5 -c $args
