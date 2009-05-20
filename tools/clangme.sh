b33a2491938774e7c510281bf75294f4b45824a0
Roger Binns <rogerb@rogerbinns.com>
2009-05-20 06:23:02 +0000

    Tweak svn update and make for clang as they are far too fragile

--- tools/clangme.sh
+++ tools/clangme.sh
@@ -18,7 +18,10 @@ export PATH=$CLANGDIR/Release/bin:$CLANGDIR/bin:$PATH
 (
     cd $CLANGDIR
     make update
-    make -j4
+    cd tools/clang
+    svn up
+    cd ..
+    make
 )
 
 $CLANGDIR/tools/clang/utils/scan-build -analyze-headers gcc -DSQLITE_ENABLE_FTS -DSQLITE_ENABLE_RTREE -DSLITE_ENABLE_ICU -DSQLITE_DEBUG -DAPSW_TESTFIXTURES -DAPSW_NO_NDEBUG -DEXPERIMENTAL -DSQLITE_THREADSAFE=1 -DAPSW_USE_SQLITE_AMALGAMATION=\"sqlite3.c\" -I. -Isqlite3 -Isrc -I/usr/include/python2.6 -c $args
