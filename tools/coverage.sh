a604b9df74d4a8f4a326a933f5211ebb1264e442
Roger Binns <rogerb@rogerbinns.com>
2009-06-10 05:48:35 +0000

    Also fixup SQLITE_THREADSAFE in my various scripts

--- tools/coverage.sh
+++ tools/coverage.sh
@@ -16,7 +16,7 @@ rm -f *.gcda *.gcov *.gcno apsw.so
 PYTHON=python # use whatever is in the path
 INCLUDEDIR=`$PYTHON -c "import distutils.sysconfig,sys; sys.stdout.write(distutils.sysconfig.get_python_inc())"`
 set -x
-gcc -pthread -fno-strict-aliasing -ftest-coverage -fprofile-arcs -g -fPIC -Wall -Wextra -DEXPERIMENTAL -DSQLITE_DEBUG -DSQLITE_THREADSAFE=1 -DAPSW_USE_SQLITE_AMALGAMATION=\"sqlite3.c\" -DAPSW_NO_NDEBUG -DAPSW_TESTFIXTURES -I$INCLUDEDIR -I. -Isqlite3 -Isrc -c src/apsw.c
+gcc -pthread -fno-strict-aliasing -ftest-coverage -fprofile-arcs -g -fPIC -Wall -Wextra -DEXPERIMENTAL -DSQLITE_DEBUG -DAPSW_USE_SQLITE_AMALGAMATION=\"sqlite3.c\" -DAPSW_NO_NDEBUG -DAPSW_TESTFIXTURES -I$INCLUDEDIR -I. -Isqlite3 -Isrc -c src/apsw.c
 gcc -pthread -ftest-coverage -fprofile-arcs -g -shared apsw.o -o apsw.so
 gcc -fPIC -shared -Isqlite3 -I. -o testextension.sqlext -Isqlite3 src/testextension.c
 set +e
