f2fff187dcb5040d73a69b030add52f90d1923f0
Roger Binns <rogerb@rogerbinns.com>
2010-10-10 21:31:31 -0700

    Bump minimum SQLite version information

--- src/apsw.c
+++ src/apsw.c
@@ -57,8 +57,8 @@ API Reference
 #include "sqlite3.h"
 #endif
 
-#if SQLITE_VERSION_NUMBER < 3007002
-#error Your SQLite version is too old.  It must be at least 3.7.2
+#if SQLITE_VERSION_NUMBER < 3007003
+#error Your SQLite version is too old.  It must be at least 3.7.3
 #endif
 
 /* system headers */
