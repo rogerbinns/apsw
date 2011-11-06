bdd3fc4a8e23afbe8d731db0a86c840495e22619
Roger Binns <rogerb@rogerbinns.com>
2011-11-05 19:42:46 -0700

    Add Google analytics to doc

--- doc/conf.py
+++ doc/conf.py
@@ -34,7 +34,7 @@ extlinks={
     
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['.templates']
+templates_path = ['_templates']
 
 # The suffix of source filenames.
 source_suffix = '.rst'
