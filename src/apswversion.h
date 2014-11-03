e2d193e8a2bc819edbfe3c8d27da35745e297529
Roger Binns <rogerb@rogerbinns.com>
2014-11-03 08:23:53 -0800

    Another minor version bump

--- src/apswversion.h
+++ src/apswversion.h
@@ -1,1 +1,1 @@
-#define APSW_VERSION "3.8.7-r1"
+#define APSW_VERSION "3.8.7.1-r1"
--- tools/checksums.py
+++ tools/checksums.py
@@ -7,6 +7,7 @@ import hashlib
 import re
 
 sqlitevers=(
+    '3080701',
     '3080700',
     '3080600',
     '3080500',
