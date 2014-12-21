c94a6adddfdb42392b00c25caf1ca39f08c1ad0f
Roger Binns <rogerb@rogerbinns.com>
2014-12-20 17:07:22 -0800

    Python 3 fix: map returns iter not list

--- tests.py
+++ tests.py
@@ -678,14 +678,14 @@ class APSW(unittest.TestCase):
             self.assertEqual(cols, c.getdescription())
             self.assertEqual(cols, tuple([d[:2] for d in c.description]))
             self.assertEqual((None,None,None,None,None), c.description[0][2:])
-            self.assertEqual(map(len, c.description), [7]*len(cols))
+            self.assertEqual(list(map(len, c.description)), [7]*len(cols))
         # check description caching isn't broken
         cols2=cols[1:4]
         for row in c.execute("select y,z,a from foo"):
             self.assertEqual(cols2, c.getdescription())
             self.assertEqual(cols2, tuple([d[:2] for d in c.description]))
             self.assertEqual((None,None,None,None,None), c.description[0][2:])
-            self.assertEqual(map(len, c.description), [7]*len(cols2))
+            self.assertEqual(list(map(len, c.description)), [7]*len(cols2))
         # execution is complete ...
         self.assertRaises(apsw.ExecutionCompleteError, c.getdescription)
         self.assertRaises(apsw.ExecutionCompleteError, lambda: c.description)
