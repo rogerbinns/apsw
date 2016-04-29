4282183c7d1c9b20b8feb23670c0ba22f9c1042b
Roger Binns <rogerb@rogerbinns.com>
2016-04-28 18:29:00 -0700

    Do get TypeError on windows

--- tests.py
+++ tests.py
@@ -5156,7 +5156,7 @@ class APSW(unittest.TestCase):
         TestFile.xRead=TestFile.xRead4
         self.assertRaises(apsw.SQLError, self.assertRaisesUnraisable, TypeError, testdb)
         TestFile.xRead=TestFile.xRead5
-        self.assertRaises(apsw.IOError, testdb)
+        self.assertRaises(apsw.IOError, self.assertMayRaiseUnraiseable, TypeError, testdb)
         TestFile.xRead=TestFile.xRead99
         testdb()
 
