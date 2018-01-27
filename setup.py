654603d071cfcd82f1cb26689059795ca487e3aa
Roger Binns <rogerb@rogerbinns.com>
2018-01-27 12:27:13 -0800

    Release bump

--- setup.py
+++ setup.py
@@ -76,7 +76,9 @@ def fixup_download_url(url):
     if ver:
         ver=int(ver.group(0))
         if ver>=3071600:
-            if ver>=3160000:
+            if ver>=3120000:
+                year="2018"
+            elif ver>=3160000:
                 year="2017"
             elif ver>=3100000:
                 year="2016"
