2fd5ef90e92e788f4d6f48f0026e68c39ed96969
adontz <adontz@gmail.com>
2019-02-27 22:00:59 +0400

    Update fixup_download_url function to reflect the latest 3.27.x SQLite download URL.
    3.27.0 was published Feb 7, 3.27.1 was published Feb 8, 3.27.2 was published Feb 25

--- setup.py
+++ setup.py
@@ -76,7 +76,9 @@ def fixup_download_url(url):
     if ver:
         ver=int(ver.group(0))
         if ver>=3071600:
-            if ver>=3220000:
+            if ver>=3270000:
+                year="2019"
+            elif ver>=3220000:
                 year="2018"
             elif ver>=3160000:
                 year="2017"
