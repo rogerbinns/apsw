7a7b1fdf692e3901f9ba4d3c813288fe5bbc6425
Roger Binns <rogerb@rogerbinns.com>
2020-02-23 14:04:46 -0800

    Fix more typos
    Fixes #279

--- tools/spellcheck.sh
+++ tools/spellcheck.sh
@@ -2,8 +2,8 @@
 
 # https://github.com/rogerbinns/apsw/pull/229
 
-git grep --color -Eniw '(paramaters|paramater|partifular|seperately|desribes|seperate|seperated|begining|targetted|libary|interpretter|entireity|publically|noone|commiting|statment|simulataneous|exection|wierd|valueable)' | grep -v tools/spellcheck.sh
+git grep --color -Eniw '(paramaters|paramater|partifular|seperately|desribes|seperate|seperated|begining|targetted|libary|interpretter|entireity|publically|noone|commiting|statment|statments|simulataneous|exection|wierd|valueable)' | grep -v tools/spellcheck.sh
 
-n=`git grep --color -Eniw '(paramaters|paramater|partifular|seperately|desribes|seperate|seperated|begining|targetted|libary|interpretter|entireity|publically|noone|commiting|statment|simulataneous|exection|wierd|valueable)' | grep -v tools/spellcheck.sh | wc -l`
+n=`git grep --color -Eniw '(paramaters|paramater|partifular|seperately|desribes|seperate|seperated|begining|targetted|libary|interpretter|entireity|publically|noone|commiting|statment|statments|simulataneous|exection|wierd|valueable)' | grep -v tools/spellcheck.sh | wc -l`
 
 if [ $n -gt 0 ] ; then exit 1 ; else exit 0 ; fi
