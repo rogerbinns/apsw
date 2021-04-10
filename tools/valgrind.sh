c4e26732c4e27bf2e2d2690104da70de1270bd92
Roger Binns <rogerb@rogerbinns.com>
2021-04-10 14:54:46 -0700

    Update for current Python
    In 8 years this stuff has got more complex and finnicky

--- tools/valgrind.sh
+++ tools/valgrind.sh
@@ -6,27 +6,20 @@
 # You should build a debug Python, with this being an example using /space/pydebug as
 # the root.
 #
-# ver=2.7.5
+# ver=3.9.4
 # mkdir /space/pydebug
 # cd /space/pydebug
-# wget -O -  http://www.python.org/ftp/python/$ver/Python-$ver.tar.bz2 | tar xfj -
+# wget -O -  https://www.python.org/ftp/python/$ver/Python-$ver.tar.xz | tar xfJ -
 # cd Python-$ver
 # # As an optimization Python keeps lists of various objects around for quick recycling
 # # instead of freeing and then mallocing.  Unfortunately that obfuscates which code
 # # was actually responsible for their existence.  Consequently we set all these to zero
 # # so that normal malloc/frees happen and valgrind can do its magic.  Before Python 2.6
 # # they all had different arbitrary names and in many cases could not be overridden.
-# # PyDict_MAXFREELIST
-# # PyTuple_MAXFREELIST
-# # PyUnicode_MAXFREELIST
-# # PySet_MAXFREELIST
-# # PyCFunction_MAXFREELIST
-# # PyList_MAXFREELIST
-# # PyFrame_MAXFREELIST
-# # PyMethod_MAXFREELIST
+#
 # s="_MAXFREELIST=0"
-# ./configure --enable-unicode=ucs4 --with-pydebug --without-pymalloc --prefix=/space/pydebug \
-# CPPFLAGS="-DPyDict$s -DPyFloat$s -DPyTuple$s -DPyUnicode$s -DPySet$s -DPyCFunction$s -DPyList$s -DPyFrame$s -DPyMethod$s"
+# ./configure --with-pydebug --with-valgrind --without-pymalloc --prefix=/space/pydebug \
+# CPPFLAGS="-DPyDict$s -DPyFloat$s -DPyTuple$s -DPyList$s -DPyFrame$s"
 #
 # make install
 #
@@ -67,10 +60,15 @@ then
     DEFS="$DEFS -DAPSW_USE_SQLITE_CONFIG=\"sqlite3/sqlite3config.h\""
 fi
 # find python
-PYTHON=${PYTHON:-python} # use whatever is in the path
-INCLUDEDIR=`$PYTHON -c "import distutils.sysconfig, sys; sys.stdout.write(distutils.sysconfig.get_python_inc())"`
-set -x
-rm -f apsw.o apsw.so
-gcc -pthread -fno-strict-aliasing  -g $opt -fPIC -W -Wall $cflags -DEXPERIMENTAL $DEFS -DAPSW_USE_SQLITE_AMALGAMATION=\"sqlite3.c\" -I$INCLUDEDIR -Isrc -I. -Isqlite3 -c src/apsw.c
-gcc -pthread  -g $opt -shared apsw.o -o apsw.so
+PYTHON=${PYTHON:-python3} # use whatever is in the path
+INCLUDEDIR=`$PYTHON -c "import sysconfig; print(sysconfig.get_path('include'))"`
+CC=`$PYTHON -c "import sysconfig; print(sysconfig.get_config_var('CC'))"`
+CFLAGS=`$PYTHON -c "import sysconfig; print(sysconfig.get_config_var('CFLAGS'))"`
+MOREFLAGS=`$PYTHON -c "import sysconfig; print(sysconfig.get_config_var('CCSHARED'))"`
+LINKER=`$PYTHON -c "import sysconfig; print(sysconfig.get_config_var('LDSHARED'))"`
+SOSUFFIX=`$PYTHON -c "import sysconfig; print(sysconfig.get_config_var('EXT_SUFFIX'))"`
+set -ex
+rm -f apsw.o apsw.*.so apsw.so
+$CC $CFLAGS $MOREFLAGS $opt $cflags -DEXPERIMENTAL $DEFS -DAPSW_USE_SQLITE_AMALGAMATION=\"sqlite3/sqlite3.c\" -I$INCLUDEDIR -Isrc -I. -c src/apsw.c
+$LINKER -g $opt apsw.o -o apsw$SOSUFFIX
 time env $apswopt valgrind $options $PYTHON $args
