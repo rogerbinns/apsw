27f672a194f7691587ae2214691337fe03dcca84
Roger Binns <rogerb@rogerbinns.com>
2021-07-30 12:49:11 -0700

    release

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.35.4-r1',
+    VERSION: '3.36.0-r1',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
