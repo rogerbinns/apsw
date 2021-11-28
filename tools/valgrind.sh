aa70ec0160aa5a1bb91aab3f01a8b53a3edf32e9
Roger Binns <rogerb@rogerbinns.com>
2021-11-28 13:43:34 -0800

    Also track origins

--- tools/valgrind.sh
+++ tools/valgrind.sh
@@ -19,9 +19,9 @@ fi
 
 if [ -z "$CALLGRIND" ]
 then
-   options="--track-fds=yes --num-callers=50 $showleaks --freelist-vol=500000000 --suppressions=`dirname $0`/sqlite3.supp"
    cflags="-DAPSW_TESTFIXTURES -DAPSW_NO_NDEBUG"
    opt="-Os -g"
+   options="--track-fds=yes --num-callers=50 $showleaks --freelist-vol=500000000 --suppressions=`dirname $0`/sqlite3.supp --track-origins=yes"
    APSW_TEST_ITERATIONS=${APSW_TEST_ITERATIONS:=150}
    apswopt="APSW_NO_MEMLEAK=t APSW_TEST_ITERATIONS=$APSW_TEST_ITERATIONS"
 else
