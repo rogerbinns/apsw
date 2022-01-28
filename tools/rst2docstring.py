6698b63eb0687014a5584ba8a788c5d394fbfd8c
Roger Binns <rogerb@rogerbinns.com>
2022-01-28 14:27:38 -0800

    Update code to do argparsing

--- tools/rst2docstring.py
+++ tools/rst2docstring.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
+"""
+This implements functionality similar to Argument Clinic.
+
+* Docstrings are available as C symbols, and then used
+* The syntax for __text_signature is used to give a partial type
+  signature
+* Argument parsing is automated
+
+Argument parsing is done differently.  Generated code is placed in the
+original source so normal tools can see it, but argument clinic then
+uses checksums to detect modifications.  We use the simpler approach
+of replacing the generated section if it differs, so git will tell if
+it was modified.
+"""
 
 import sys
 import os
@@ -23,7 +37,7 @@ skipseen = set()
 def process_file(name: str) -> list:
     "Read one rst file and extract docstrings"
     items = []
-    current : List[str] = []
+    current: List[str] = []
 
     def do_current():
         nonlocal current
@@ -49,7 +63,7 @@ def process_file(name: str) -> list:
     return items
 
 
-def classify(doc: list) -> Union[dict,  None]:
+def classify(doc: list) -> Union[dict, None]:
     "Process docstring and ignore or update details"
     line = doc[0]
     assert line.startswith(".. ")
@@ -90,41 +104,62 @@ def classify(doc: list) -> Union[dict,  None]:
     doc = [f"{ line }\n" for line in textwrap.dedent("".join(doc)).strip().split("\n")]
 
     symbol = make_symbol(name)
-
-    return {"name": name, "symbol": symbol, "signature": signature, "doc": doc}
+    return {
+        "name": name,
+        "symbol": symbol,
+        "signature_original": signature,
+        "signature": analyze_signature(signature) if signature else [],
+        "doc": doc
+    }
 
 
 def make_symbol(n: str) -> str:
     "Returns C symbol name"
     n = n[0].upper() + n[1:]
-    n = n.replace(".", "_").replace("__", "_")
-    return n + "_DOC"
+    n = n.replace(".", "_").replace("__", "_").replace("__", "_")
+    return n.rstrip("_")
 
 
-def fixup(item: dict, eol: str) -> str:
-    "Return docstring lines after making safe for C"
-
+def cppsafe(lines: List[str], eol: str) -> str:
     def backslash(l):
         return l.replace('"', '\\"').replace("\n", "\\n")
-
-    lines = item["doc"]
-    if item["signature"]:
-        # cpython can't handle the return type info
-        sig = simplify_signature(item["signature"])
-        func = item["name"].split(".")[1]
-        if func != "__init__":
-            assert sig[0] == "("
-            sig = "($self, " + sig[1:]
-        lines = [f'''{ func }{ sig }\n--\n\n{ item["name"] }{ item["signature"] }\n\n'''] + lines
 
     res = "\n".join(f'''"{ backslash(line) }"{ eol }''' for line in lines)
     res = res.strip().strip("\\").strip()
     return res
 
-def simplify_signature(s: str) -> str:
-    "Remove information CPython won't include in __text_signature__"
-    # it doesn't like types, defaults
-    s = s.split("->")[0].strip() # ... or return information
+
+def fixup(item: dict, eol: str) -> str:
+    "Return docstring lines after making safe for C"
+    lines = item["doc"]
+    if item["signature"]:
+        # cpython can't handle the arg or return type info
+        sig = simple_signature(item["signature"])
+        func = item["name"].split(".")[1]
+        lines = [f'''{ func }{ sig }\n--\n\n{ item["name"] }{ item["signature_original"] }\n\n'''] + lines
+
+    return cppsafe(lines, eol)
+
+
+def simple_signature(signature: List[dict]) -> str:
+    "Return signature simple enough to be accepted for __text_signature__"
+    res = ["$self"]
+    for param in signature:
+        if param["name"] == "return":
+            continue
+        res.append(param["name"])
+    res.append("/")
+    return "(" + ",".join(res) + ")"
+
+
+def analyze_signature(s: str) -> List[dict]:
+    "parse signature returning info about each item"
+    res = []
+    if "->" in s:
+        s, rettype = [ss.strip() for ss in s.split("->", 1)]
+    else:
+        rettype = "None"
+    res.append({"name": "return", "type": rettype})
 
     assert s[0] == "(" and s[-1] == ")"
 
@@ -132,15 +167,29 @@ def simplify_signature(s: str) -> str:
     nest_start = "[("
     nest_end = "])"
 
-    params = []
     pos = 1
     nesting = 0
     name = ""
+    after_name = ""
     skip_to_next = False
 
-    for pos in range(1, len(s)-1):
+    def add_param():
+        nonlocal name, after_name
+        p = {"name": name}
+        after_name = [a.strip() for a in after_name.strip().lstrip(":").split("=", 1)]
+        if len(after_name) > 1:
+            after_name, default = after_name
+        else:
+            after_name, default = after_name[0], None
+        p["type"] = after_name
+        p["default"] = default
+        res.append(p)
+        name = after_name = ""
+
+    for pos in range(1, len(s) - 1):
         c = s[pos]
         if c in nest_start or nesting:
+            after_name += c
             if c in nest_start:
                 nesting += 1
                 continue
@@ -151,58 +200,159 @@ def simplify_signature(s: str) -> str:
 
         if c == ',':
             assert name
-            params.append(name)
-            name = ""
+            add_param()
             skip_to_next = False
+            continue
+
+        if skip_to_next:
+            after_name += c
             continue
 
         if name and not (name + c).isidentifier() and not (name[0] == "*" and (name[1:] + c).isidentifier()):
             skip_to_next = True
-            continue
-
-        if skip_to_next:
+            after_name = ""
             continue
 
         if c.isidentifier() or (not name and c in "/*"):
             name += c
 
     if name:
-        params.append(name)
+        add_param()
 
-    if "/" not in params and not any(p.startswith("*") for p in params):
-        params.append("/")
+    return res
 
-    return f"({ ', '.join(params) })"
 
-items = []
-for fname in sys.argv[2:]:
-    items.extend(process_file(fname))
+def check_and_update(symbol: str, code: str):
+    for fn in glob.glob("src/*.c"):
+        orig = open(fn, "r").read()
+        if symbol not in orig:
+            continue
+        return check_and_update_file(fn, symbol, code)
+    else:
+        raise ValueError(f"Failed to find code with { symbol }")
 
-out = io.StringIO()
-print("/* This file is generated by rst2docstring */\n", file=out)
-method, mid, eol, end = "static const char * const ", " = ", "", ";"
-method, mid, eol, end = "#define ", " ", " \\", ""
-for item in sorted(items, key=lambda x: x["symbol"]):
-    print(f"""{ method } { item["symbol"] }{ mid }{ fixup( item, eol) } { end }\n""", file=out)
 
-outval = out.getvalue()
-if not os.path.exists(sys.argv[1]) or open(sys.argv[1]).read() != outval:
-    open(sys.argv[1], "w").write(outval)
+def check_and_update_file(filename: str, symbol: str, code: str):
+    lines = open(filename, "r").read().split("\n")
+    insection = False
+    for lineno, line in enumerate(lines):
+        if line.strip() == "{":
+            lineopen = lineno
+        elif line.strip() == "}" and insection:
+            lineclose = lineno
+            break
+        elif symbol in line:
+            insection = True
+    else:
+        raise ValueError(f"{ symbol } not found in { filename }")
 
-symbols = sorted([item["symbol"] for item in items])
+    lines = lines[:lineopen] + code.split("\n") + lines[lineclose + 1:]
 
-code = "\n".join(open(fn).read() for fn in glob.glob("src/*.c"))
+    new = "\n".join(lines)
+    if not new.endswith("\n"):
+        new += "\n"
+    replace_if_different(filename, new)
 
-if skip != skipseen:
-    print("in skip, but not seen\n")
-    for s in skip:
-        if s not in skipseen:
-            print("  ", s)
-    print()
 
-if any(s not in code for s in symbols):
-    print("Unreferenced doc\n")
-    for s in symbols:
-        if s not in code:
-            print("  ", s)
-    sys.exit(2)
+def replace_if_different(filename: str, contents: str) -> None:
+    if not os.path.exists(filename) or open(filename).read() != contents:
+        print(f"{ 'Creating' if not os.path.exists(filename) else 'Updating' } { filename }")
+        open(filename, "w").write(contents)
+
+
+def do_argparse(item):
+    for param in item["signature"]:
+        if not param["type"].strip():
+            sys.exit(f"{ item['name'] } param { param } has no type from { item['signature_original'] }")
+    res = [f"#define { item['symbol'] }_CHECK do {{ \\"]
+
+    fstr = ""
+    optional = False
+    argnames = []
+
+    for param in item["signature"]:
+        if param["name"] == "return":
+            continue
+        if param["type"] == "str":
+            type = "const char *"
+            kind = "s"
+        else:
+            assert False, f"Don't know how to handle type for { item ['name'] } param { param }"
+
+        res.append(f"  assert(__builtin_types_compatible_p(typeof({ param['name'] }),typeof({ type }))); \\")
+
+        if not optional and param["default"]:
+            fstr.append("|")
+            optional = True
+
+        fstr += kind
+        argnames.append(param["name"])
+
+    res.append("} while(0)\n")
+
+    code = f"""\
+  {{
+    static const char *kwlist[] = {{{ ",".join('"' + a + '"' for a in argnames) }, NULL}};
+    { item['symbol'] }_CHECK;
+    if (!PyArg_ParseTupleAndKeywords(args, kwds, "{ fstr }:" { item['symbol'] }_USAGE, kwlist, { ",".join("&" + a for a in argnames) }))
+      return NULL;
+  }}"""
+
+    usage = f"{ item['name'] }{ item['signature_original'] }".replace('"', '\\"')
+    res.insert(0, f"""#define { item['symbol'] }_USAGE "{ usage }"\n""")
+
+    check_and_update(f"{ item['symbol'] }_CHECK", code)
+
+    res = "\n".join(res) + "\n"
+    return res
+
+
+if __name__ == '__main__':
+    items = []
+    for fname in sys.argv[2:]:
+        items.extend(process_file(fname))
+
+    allcode = "\n".join(open(fn).read() for fn in glob.glob("src/*.c"))
+
+    missing = []
+
+    out = io.StringIO()
+    print("""/* This file is generated by rst2docstring */
+
+#ifndef __GNUC__
+#define __builtin_types_compatible_p(x,y) (1)
+#endif
+""",
+          file=out)
+    method, mid, eol, end = "#define ", " ", " \\", ""
+    for item in sorted(items, key=lambda x: x["symbol"]):
+        print(f"""{ method } { item["symbol"] }_DOC{ mid }{ fixup( item, eol) } { end }\n""", file=out)
+        if f"{ item['symbol'] }_CHECK" in allcode:
+            print(do_argparse(item), file=out)
+        else:
+            if any(param["name"] != "return"
+                   for param in item["signature"]) and not any(param["name"].startswith("*")
+                                                               for param in item["signature"]):
+                missing.append(item["name"])
+
+    outval = out.getvalue()
+    replace_if_different(sys.argv[1], outval)
+
+    symbols = sorted([item["symbol"] for item in items])
+
+    if skip != skipseen:
+        print("in skip, but not seen\n")
+        for s in skip:
+            if s not in skipseen:
+                print("  ", s)
+        print()
+
+    if any(s not in allcode for s in symbols):
+        print("Unreferenced doc\n")
+        for s in symbols:
+            if s not in code:
+                print("  ", s)
+        sys.exit(2)
+
+    if missing:
+        print("Not argparse checked", missing)
