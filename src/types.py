f186c9a75966d0affb0b8375f4f065947456e337
Roger Binns <rogerb@rogerbinns.com>
2022-06-14 15:19:51 -0700

    Starter built in types for annotation

---
+++ src/types.py
@@ -0,0 +1,6 @@
+
+from typing import Union, Tuple, List, Optional, Callable, Any, Dict, Iterator, Sequence, Literal, Set
+from array import array
+
+SQLiteValue = Union[None, int, float, bytes, str]
+
