1bee20fcd9a801b3b5b74bd7462bcb57808d08c6
Roger Binns <rogerb@rogerbinns.com>
2022-12-14 14:11:12 -0800

    Account for more of the sqlite apis

--- tools/find_unwrapped_apis.py
+++ tools/find_unwrapped_apis.py
@@ -21,7 +21,6 @@ with tempfile.NamedTemporaryFile() as f:
 # functions we won't wrap
 nowrap = {
     # memory allocation
-    "sqlite3_malloc",
     "sqlite3_msize",
     "sqlite3_realloc",
     "sqlite3_realloc64",
@@ -36,6 +35,10 @@ nowrap = {
     # we use the -64 suffix versions
     "sqlite3_changes",
     "sqlite3_total_changes",
+    "sqlite3_malloc",
+    "sqlite3_column_int",
+    "sqlite3_bind_int",
+    "sqlite3_value_int",
 
     # not useful
     "sqlite3_next_stmt",
@@ -47,17 +50,70 @@ nowrap = {
     "sqlite3_vmprintf",
     "sqlite3_vsnprintf",
     "sqlite3_stmt_busy",
+    "sqlite3_keyword_check",  # sqlite3_keyword_name is used
+    "sqlite3_compileoption_used",  # sqlite3_compileoption_get is used
+    "sqlite3_uri_key",
+    "sqlite3_set_auxdata",
+    "sqlite3_get_auxdata",
+    "sqlite3_value_dup",
+    "sqlite3_value_encoding",
+    "sqlite3_value_free",
+    "sqlite3_value_frombind",
+    "sqlite3_value_numeric_type",
+    "sqlite3_value_pointer",
+    "sqlite3_bind_parameter_index",  # we don't need to do this direction
+    "sqlite3_context_db_handle",
+    "sqlite3_create_filename",
+    "sqlite3_free_filename",
+    "sqlite3_filename_database",
+    "sqlite3_database_file_object",
+    "sqlite3_db_handle",
+    "sqlite3_errcode",  # extended is used
+    "sqlite3_errstr",
+    "sqlite3_get_table",
+    "sqlite3_free_table",
+    "sqlite3_result_value",
+    "sqlite3_auto_extension",  # connection_hooks are used
+    "sqlite3_cancel_auto_extension",
+    "sqlite3_reset_auto_extension",
 
-    # deprecated but not marked as such
+    # deprecated but not marked as such (v2 exists)
     "sqlite3_trace",
+    "sqlite3_wal_checkpoint",
+    "sqlite3_create_collation",
+    "sqlite3_create_module",
 
     # windows only, no one has asked for it
     "sqlite3_win32_set_directory",
     "sqlite3_win32_set_directory8",
+
+    # we don't do subtypes / pointer
+    "sqlite3_value_subtype",
+    "sqlite3_result_subtype",
+    "sqlite3_bind_pointer",
+    "sqlite3_result_pointer",
+
+    # requires non-default compile options
+    "sqlite3_normalized_sql",
+    "sqlite3_test_control",
+    "sqlite3_stmt_scanstatus",
+    "sqlite3_stmt_scanstatus_reset",
+    "sqlite3_snapshot_cmp",
+    "sqlite3_snapshot_free",
+    "sqlite3_snapshot_get",
+    "sqlite3_snapshot_open",
+    "sqlite3_snapshot_recover",
+    "sqlite3_preupdate_blobwrite",
+    "sqlite3_preupdate_count",
+    "sqlite3_preupdate_depth",
+    "sqlite3_preupdate_hook",
+    "sqlite3_preupdate_new",
+    "sqlite3_preupdate_old",
 }
 
 for f in sorted(functions):
-    res = subprocess.run(["git", "grep", "-wFq", f])
+    # we have to exclude this file ...
+    res = subprocess.run(["git", "grep", "-wFq", f, "--", ":^tools/find_unwrapped_apis.py"])
     assert res.returncode >= 0
     if res.returncode == 0:  # found
         if f in nowrap:
