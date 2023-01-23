bdc0961ae9259cd85661e8c0b61ee175ae4855bd
Roger Binns <rogerb@rogerbinns.com>
2023-01-23 10:29:43 -0800

    Cope with SQLite setting all colUsed bits

--- apsw/ext.py
+++ apsw/ext.py
@@ -376,7 +376,9 @@ def index_info_to_dict(o: apsw.IndexInfo,
                 aconstraint["iColumn"]]
         for aorderby in res["aOrderBy"]:
             aorderby["iColumn_name"] = rowid_name if aorderby["iColumn"] == -1 else column_names[aorderby["iColumn"]]
-        res["colUsed_names"] = set(column_names[i] for i in o.colUsed)
+        # colUsed has all bits set when SQLite just wants the whole row
+        # eg when doing an update
+        res["colUsed_names"] = set(column_names[i] for i in o.colUsed if i<len(column_names))
         if 63 in o.colUsed:  # could be one or more of the rest - we add all
             res["colUsed_names"].update(column_names[63:])
 
