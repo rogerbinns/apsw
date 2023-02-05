5b45829ad63d4a8c73cbf06679d826e7f0069c46
Roger Binns <rogerb@rogerbinns.com>
2023-02-05 11:04:58 -0800

    Simplify

--- tools/coverageanalyser.py
+++ tools/coverageanalyser.py
@@ -6,21 +6,19 @@
 
 import glob
 import os
-import sys
-
-out = sys.stdout.write
 
 
-def output(filename, percent, total):
+def output(filename: str, executed: int, total: int) -> None:
     # Python bug "% 3.2f" doesn't behave correctly (100.00 is formatted with leading space!)
-    op = ["%-40s" % (filename, ), "% 3.2f%%" % (percent, ), " of % 6d" % (total, )]
+    percent = 100 * executed / max(total, 1)
+    op = [f"{filename:40}", f"{executed:6,}", "  /", f"{total:6,}", "\t    ", "% 3.2f%%" % (percent, )]
     if percent == 100:
-        op[1] = "100.00%"
-    out("".join(op) + "\n")
+        op[-1] = "100.00%"
+    print("".join(op))
 
 
-linesexecuted = 0
-linestotal = 0
+lines_executed = 0
+lines_total = 0
 
 names = glob.glob("src/*.c.gcov")
 names.sort()
@@ -28,20 +26,20 @@ names.sort()
 for f in names:
     if f.startswith("sqlite3"):
         continue
-    fileexec = 0
-    filetotal = 0
+    file_exec = 0
+    file_total = 0
     for line in open(f, "rt"):
         if ":" not in line: continue
         line = line.split(":", 1)[0].strip()
         if line == "-":
             continue
         if line != "#####":
-            linesexecuted += 1
-            fileexec += 1
-        linestotal += 1
-        filetotal += 1
+            lines_executed += 1
+            file_exec += 1
+        lines_total += 1
+        file_total += 1
     n = os.path.splitext(f)[0]
-    output(n, fileexec * 100.0 / filetotal, filetotal)
+    output(n, file_exec, file_total)
 
-out("\n")
-output("Total", linesexecuted * 100.0 / (linestotal if linestotal else 1), linestotal)
+print("\n")
+output("Total", lines_executed, lines_total)
