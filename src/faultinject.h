6c45bcae2238b4f5f96e87f5b9db4591475b484e
Roger Binns <rogerb@rogerbinns.com>
2023-02-14 20:28:10 -0800

    last hand written version before going to generated

--- src/faultinject.h
+++ src/faultinject.h
@@ -1,4 +1,5 @@
 
+#ifndef APSW_FAULT_INJECT_INCLUDED
 
 typedef enum
 {
@@ -9,6 +10,14 @@ typedef enum
 
 static FaultInjectControlVerb
 APSW_FaultInjectControl(int is_call, const char *faultfunction, const char *filename, const char *funcname, int linenum, const char *args, PyObject **obj);
+#define APSW_FAULT_INJECT_INCLUDED
+#endif
+
+#ifdef APSW_FAULT_INJECT_OFF
+#undef PySet_New
+#undef convert_value_to_pyobject
+#undef APSW_FAULT_INJECT_OFF
+#else
 
 #define PySet_New(...)                                                                                      \
     ({                                                                                                      \
@@ -33,3 +42,27 @@ APSW_FaultInjectControl(int is_call, const char *faultfunction, const char *file
         PyGILState_Release(gilstate);                                                                       \
         _res;                                                                                               \
     })
+
+#define convert_value_to_pyobject(...)                                                                                                     \
+    ({                                                                                                                                     \
+        PyObject *_res = 0;                                                                                                                \
+        PyGILState_STATE gilstate = PyGILState_Ensure();                                                                                   \
+        switch (APSW_FaultInjectControl(1, "convert_value_to_pyobject", __FILE__, __func__, __LINE__, #__VA_ARGS__, &_res))                \
+        {                                                                                                                                  \
+        case FICProceed:                                                                                                                   \
+            assert(_res == 0);                                                                                                             \
+            _res = convert_value_to_pyobject(__VA_ARGS__);                                                                                 \
+            break;                                                                                                                         \
+        case FICProceed_And_Call_With_Result:                                                                                              \
+            assert(_res == 0);                                                                                                             \
+            _res = convert_value_to_pyobject(__VA_ARGS__);                                                                                 \
+            APSW_FaultInjectControl(0, "convert_value_to_pyobject", __FILE__, __func__, __LINE__, #__VA_ARGS__, &_res); /* fallthrough  */ \
+        case FICReturnThis:                                                                                                                \
+            assert(_res || PyErr_Occurred());                                                                                              \
+            assert(!(_res && PyErr_Occurred()));                                                                                           \
+            break;                                                                                                                         \
+        }                                                                                                                                  \
+        PyGILState_Release(gilstate);                                                                                                      \
+        _res;                                                                                                                              \
+    })
+#endif
