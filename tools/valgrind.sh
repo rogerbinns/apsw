40f42a87f706505546e4253138a016dc75d48893
Roger Binns <rogerb@rogerbinns.com>
2023-04-10 09:47:01 -0700

    Default to one recursion because that is what I usually want

--- tools/valgrind.sh
+++ tools/valgrind.sh
@@ -17,7 +17,7 @@ then
    cflags="-DAPSW_TESTFIXTURES -DAPSW_NO_NDEBUG"
    opt=""
    options="--track-fds=yes --num-callers=50 $showleaks --freelist-vol=500000000 --suppressions=`dirname $0`/sqlite3.supp --track-origins=yes"
-   APSW_TEST_ITERATIONS=${APSW_TEST_ITERATIONS:=150}
+   APSW_TEST_ITERATIONS=${APSW_TEST_ITERATIONS:=1}
    apswopt="APSW_NO_MEMLEAK=t APSW_TEST_ITERATIONS=$APSW_TEST_ITERATIONS"
    APSW_VALGRIND=t
    export APSW_VALGRIND
