6c9efb1e4afe728ee9478409cff2dc20cbd04c12
Roger Binns <rogerb@rogerbinns.com>
2023-04-13 08:44:49 -0700

    Add missing _ in rst

--- src/vfs.c
+++ src/vfs.c
@@ -12,7 +12,7 @@ Virtual File System (VFS)
 *************************
 
 SQLite 3.6 added `VFS functionality
-<https://sqlite.org/c3ref/vfs.html>`_ which defines the interface
+<https://sqlite.org/c3ref/vfs.html>`__ which defines the interface
 between the SQLite core and the underlying operating system. The
 majority of the functionality deals with files. APSW exposes this
 functionality letting you provide your own routines. You can also
