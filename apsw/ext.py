4480a3b4ee5cc793def6537bbc9e882ac7c9bb1a
Roger Binns <rogerb@rogerbinns.com>
2023-08-08 11:35:16 -0700

    Simpler repr_invalid check
    format_sql_value doesn't do validity checks (eg if integer
    is larger than signed 64 bit, or strings >1GB) so there
    is no value in using it.

--- apsw/ext.py
+++ apsw/ext.py
@@ -1162,18 +1162,14 @@ def make_virtual_module(db: apsw.Connection,
             def Column(self, which: int) -> apsw.SQLiteValue:
                 if which >= self.num_columns:
                     return self.hidden_values[which - self.num_columns]
-                v: Any = None
                 if self.access == VTColumnAccess.By_Index:
                     v = self.current_row[which]
                 elif self.access == VTColumnAccess.By_Name:
                     v = self.current_row[self.columns[which]]
                 elif self.access == VTColumnAccess.By_Attr:
                     v = getattr(self.current_row, self.columns[which])
-                if self.repr_invalid:
-                    try:
-                        apsw.format_sql_value(v)
-                    except TypeError:
-                        v = repr(v)
+                if self.repr_invalid and v is not None and not isinstance(v, (int, float, str, bytes)):
+                    v = repr(v)
                 return v  # type: ignore[no-any-return]
 
             def Next(self) -> None:
