17803aed832766f7cc49b8bdb9b872776740412e
Roger Binns <rogerb@rogerbinns.com>
2023-08-10 08:00:38 -0700

    RecursionError testing may give related exception

--- apsw/tests.py
+++ apsw/tests.py
@@ -1138,7 +1138,7 @@ class APSW(unittest.TestCase):
         a = VFSA()
         b = VFSB()
         sys.setrecursionlimit(200)
-        print("A message about RecursionError is possible, and what is being tested", file=sys.stderr)
+        print("A message due to RecursionError is possible, and what is being tested", file=sys.stderr)
         self.assertRaises((apsw.SQLError, RecursionError),
                           self.assertRaisesUnraisable, (apsw.SQLError, RecursionError),
                           apsw.Connection,
