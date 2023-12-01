de0bbb85838d72c7ecb71670d22d6878659d6c36
Roger Binns <rogerb@rogerbinns.com>
2023-11-30 17:20:05 -0800

    Updating fts doc

--- src/fts.c
+++ src/fts.c
@@ -9,10 +9,78 @@ SQLite provides the `FTS5 extension <https://www.sqlite.org/fts5.html>`__
 as the implementation.  It is enabled by default in :ref:`PyPI <pypi>`
 installs.
 
+Reading
+=======
+
+https://hsivonen.fi/string-length/
+
+https://www.unicode.org/reports/tr29/
+
+https://www.nltk.org/
+
+
+Key Concepts
+============
+
+Searching
+
+  SQL is based around the entire contents of a value.  You can test
+  for equality, you can do greater or less then, you can build indices
+  to improve performance, you can do joins between tables on the values,
+  and more.
+
+  But you can't (practically) do that on a subset of a value, especially
+  text.  You can't ask which rows/columns/values contain certain words,
+  and you can't do searches for content like you can in a web browser.
+  This is the functionality that full text search provides.
+
+Tokens
+
+  Values first need to broken down into discrete units, called tokens.
+  Most commonly these would correspond to words in English, but they don't
+  have to be.  Tokens are the unit that full text search work with,
+  with values considered to be a sequence of tokens.
+
+Full Text Index
+
+  FTS5 builds an index where a token can be looked up, and which rows
+  and columns containing it are returned including their position in
+  that column value.  So if you search for "hello world" and "hello"
+  is at position 17 in a particular row/column, and "world" is at
+  position 18 you now have a match.  FTS5 lets you include "NEAR"
+  in queries letting their positions be further apart and still be
+  a match.
+
+  Building the index can be quite time consuming, and it can take
+  quite a lot of storage space.  But it is fast to use.
+
+Ranking
+
+  Once matches are found, you want the most relevant ones first.
+  A ranking function is used to assign each match a numerical score, so
+  that can value can be used for sorting.  Ranking functions try
+  to take into account how rare the tokens are (English content has
+  "the" all over the place), and other factors based on the function
+  like if the tokens are in headings, and how many tokens are in the
+  content it was found in.
+
+Stemming
+
+
+
+Unicode
+Unicode Codepoints
+UTF8
+Unicode Categories
+Combining and Modifiers
+Normalization
+Case
+
+
 Tokenizers
 ==========
 
-* Convert bytes into a seuqnece of tokens
+* Convert bytes into a sequence of tokens
 * Get existing :meth:`Connection.fts5_tokenizer`
 * register your own :meth:`Connection.register_fts5_tokenizer`
 
@@ -21,8 +89,17 @@ Tokenizers
 
 
 * Normalization
-* byte offsets
 
+.. _byte_offsets:
+
+byte offsets
+------------
+
+* into original utf8 (ie not changed/normalized)
+* start is first byte
+* end is first byte **after** token like python does with :class:`range`
+* end minus start is length of token in bytes
+* must be complete utf8, errors if middle of a utf8 byte making up codepoint
 
 */
 
