9bf85c8a84cfad33c08bb6bf5cbaf3aabf59ca5a
Roger Binns <rogerb@rogerbinns.com>
2023-12-27 16:56:30 -0800

    Handle where we turn all errors into NOFTS5Error

--- tools/fi.py
+++ tools/fi.py
@@ -584,6 +584,12 @@ class Tester:
                 self.expect_exception.append(MemoryError)
                 return 0
 
+            # we only use this to get fts5api and always claim it was because fts5
+            # is not present
+            if fname in {"sqlite3_prepare", "sqlite3_bind_pointer"}:
+                self.expect_exception.append(apsw_attr("NoFTS5Error"))
+                return self.apsw_attr("SQLITE_ERROR")
+
             if fname.startswith("sqlite3_"):
                 self.expect_exception.append(apsw_attr("TooBigError"))
                 return self.apsw_attr("SQLITE_TOOBIG")
