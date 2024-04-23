f43ee10074f9f69af38aec1a7288c797ee8f5fa2
Roger Binns <rogerb@rogerbinns.com>
2024-04-23 08:54:08 -0700

    Simplify category_name per item macro

--- src/unicode.c
+++ src/unicode.c
@@ -1073,6 +1073,32 @@ line_next_break(PyObject *Py_UNUSED(self), PyObject *const *fast_args, Py_ssize_
   return PyLong_FromSsize_t(it.pos);
 }
 
+static void
+add_string_to_tuple(PyObject **tuple, const char *string)
+{
+  if (!*tuple)
+  {
+    *tuple = PyTuple_New(0);
+    if (!*tuple)
+      goto error;
+  }
+  PyObject *tmpstring = PyUnicode_FromString(string);
+  if (!tmpstring)
+    goto error;
+
+  if (0 != _PyTuple_Resize(tuple, 1 + PyTuple_GET_SIZE(*tuple)))
+  {
+    Py_CLEAR(tmpstring);
+    goto error;
+  }
+  PyTuple_SET_ITEM(*tuple, PyTuple_GET_SIZE(*tuple) - 1, tmpstring);
+  return;
+
+error:
+  if (*tuple)
+    Py_CLEAR(*tuple);
+}
+
 #define category_name_KWNAMES "which", "codepoint"
 static PyObject *
 category_name(PyObject *Py_UNUSED(self), PyObject *const *fast_args, Py_ssize_t fast_nargs, PyObject *fast_kwnames)
@@ -1091,34 +1117,12 @@ category_name(PyObject *Py_UNUSED(self), PyObject *const *fast_args, Py_ssize_t 
      we resize during the rare occasions when there are more than one */
 
 #define X(v)                                                                                                           \
-  do                                                                                                                   \
+  if ((val & v) == v)                                                                                                  \
   {                                                                                                                    \
-    if ((val & v) == v)                                                                                                \
-    {                                                                                                                  \
-      PyObject *tmpstring = PyUnicode_FromString(#v);                                                                  \
-      if (!tmpstring)                                                                                                  \
-        goto error;                                                                                                    \
-      if (!res)                                                                                                        \
-      {                                                                                                                \
-        res = PyTuple_New(1);                                                                                          \
-        if (!res)                                                                                                      \
-        {                                                                                                              \
-          Py_CLEAR(tmpstring);                                                                                         \
-          goto error;                                                                                                  \
-        }                                                                                                              \
-        PyTuple_SET_ITEM(res, 0, tmpstring);                                                                           \
-      }                                                                                                                \
-      else                                                                                                             \
-      {                                                                                                                \
-        if (0 != _PyTuple_Resize(&res, 1 + PyTuple_GET_SIZE(res)))                                                     \
-        {                                                                                                              \
-          Py_CLEAR(tmpstring);                                                                                         \
-          goto error;                                                                                                  \
-        }                                                                                                              \
-        PyTuple_SET_ITEM(res, PyTuple_GET_SIZE(res) - 1, tmpstring);                                                   \
-      }                                                                                                                \
-    }                                                                                                                  \
-  } while (0);
+    add_string_to_tuple(&res, #v);                                                                                     \
+    if (!res)                                                                                                          \
+      return NULL;                                                                                                     \
+  }
 
   if (0 == strcmp(which, "grapheme"))
   {
@@ -1148,14 +1152,12 @@ category_name(PyObject *Py_UNUSED(self), PyObject *const *fast_args, Py_ssize_t 
   else
   {
     PyErr_Format(PyExc_ValueError,
-                 "Unknown which parameter \"%s\" - should be one of grapheme, word, sentence, line_break, category", which);
+                 "Unknown which parameter \"%s\" - should be one of grapheme, word, sentence, line_break, category",
+                 which);
     Py_CLEAR(res);
   }
 
   return res;
-error:
-  Py_CLEAR(res);
-  return NULL;
 }
 
 static PyObject *
