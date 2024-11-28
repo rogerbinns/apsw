f3720574e6c763289d0ea72a523066089e3e4080
Roger Binns <rogerb@rogerbinns.com>
2024-11-28 15:05:05 -0800

    More hacking
    Fraction used to avoid float sum inaccuracies
    
    Text is displayed
    
    Doesn't respond to touch on mobile

--- makesvg.py
+++ makesvg.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 import dis
 
-import apsw, pprint, sys, apsw.ext, math, apsw.shell
+import apsw, pprint, sys, apsw.ext, math, apsw.shell, os, html, math
+from fractions import Fraction
 
 
 con = apsw.Connection(sys.argv[1])
@@ -79,58 +80,110 @@ def slice(id: str, start_angle: float, end_angle: float, start_distance: float, 
     ds = " ".join(d)
 
     fill = colour_for_angle((start_angle + end_angle) / 2)
-    return f"""<path id="{id}" d="{ds}" stroke="black" fill="{fill}" stroke-width="1"/>"""
+    return f"""<path id="{id}" d="{ds}" stroke="black" fill="{fill}" stroke-width="1px"/>"""
 
-def text(id: str, usage)->str:
-    return f"""<text id="{id}">{usage}</text>"""
+
+def text(pos: tuple[float, float], id: str, name: str, ring: int, usage) -> str:
+    x, y = c(pos[0]), c(pos[1])
+    e = html.escape
+    res = f"""<text id="{id}" x="{x}" y="{y}" class="infobox">"""
+    res += f"""<tspan x="{x}" dy="-2em" class="name">{e(name)}</tspan>"""
+    if ring == 0:
+        assert isinstance(usage, apsw.ext.DatabasePageUsage)
+        total = storage(usage.pages_total * usage.page_size)
+        used = storage(usage.pages_used * usage.page_size)
+        res += f"""<tspan x="{x}" dy="1em">{used} / {total}</tspan>"""
+        res += f"""<tspan x="{x}" dy="1em">{len(usage.tables):,} tables</tspan>"""
+        res += f"""<tspan x="{x}" dy="1em">{len(usage.indices):,} indices</tspan>"""
+    else:
+        if ring == 2:
+            kind = "table" if usage.tables else "index"
+            res += f"""<tspan x="{x}" dy="1em">({kind})</tspan>"""
+        size = storage(usage.pages_used * usage.page_size)
+        res += f"""<tspan x="{x}" dy="1em">{size}</tspan>"""
+        if ring == 1:
+            res += f"""<tspan x="{x}" dy="1em">{len(usage.tables):,} tables</tspan>"""
+            res += f"""<tspan x="{x}" dy="1em">{len(usage.indices):,} indices</tspan>"""
+    res += """</text>"""
+    return res
+
+
+def storage(v):
+    if not v:
+        return "0"
+    power = math.floor(math.log(v, 1024))
+    suffix = ["B", "KB", "MB", "GB", "TB", "PB", "EB"][int(power)]
+    if suffix == "B":
+        return f"{v}B"
+    return f"{v / 1024**power:.1f}".rstrip(".0") + suffix
 
 
 # controls how much whitespace is around the edges
-OVERSCAN = 1.2
+OVERSCAN = 1.05
 header = f"""<svg xmlns="http://www.w3.org/2000/svg" viewBox="-{round(RADIUS*OVERSCAN)} {-round(RADIUS*OVERSCAN)} {round(RADIUS*OVERSCAN*2)} {round(RADIUS*OVERSCAN*2)}">"""
 footer = "</svg>"
 
 # maps which element hovering over causes a response on
-hover_response :dict[str,str]= {}
+hover_response: dict[str, str] = {}
 
 id_counter = 0
+
+
 def next_id():
     global id_counter
-    id_counter+=1
+    id_counter += 1
     return f"id{id_counter}"
 
+
 out = [header]
+# z-order is based on output order so texts go last
+texts: list[str] = []
 
 # inner summary circle
 id = next_id()
 out.append(f"""<circle id="{id}" r="{c(.3)}" fill="#777"/>""")
 
-resp=next_id()
-out.append(text(resp, root))
-hover_response[id]=resp
+resp = next_id()
+texts.append(text(pos_for_angle(0, 0), resp, os.path.basename(sys.argv[1]), 0, root))
+hover_response[id] = resp
 
 
-
-ring1_start = 0
+start = Fraction()
 for name, usage in group.items():
-    ring1_proportion = usage.pages_used / root.pages_total
-    id=next_id()
-    out.append(slice(id, ring1_start, ring1_start + ring1_proportion, 1 / 3, 0.6))
-    ring2_start = ring1_start
-    ring1_start += ring1_proportion
+    ring1_proportion = Fraction(usage.pages_used, root.pages_total)
+    id = next_id()
+    out.append(slice(id, float(start), float(start + ring1_proportion), 1 / 3, 0.6))
+    resp = next_id()
+    texts.append(text(pos_for_angle(float(start + ring1_proportion / 2), (1 / 3 + 0.6) / 2), resp, name, 1, usage))
+    hover_response[id] = resp
+    ring2_start = start
+    start += ring1_proportion
 
     for child in sorted(usage.tables + usage.indices):
         usage2 = each[child]
-        ring2_proportion = usage2.pages_used / root.pages_total
-        id=next_id()
-        out.append(slice(id, ring2_start, ring2_start + ring2_proportion, 2 / 3, 1.0))
+        ring2_proportion = Fraction(usage2.pages_used, root.pages_total)
+        id = next_id()
+        out.append(slice(id, float(ring2_start), float(ring2_start + ring2_proportion), 2 / 3, 1.0))
+        resp = next_id()
+        texts.append(
+            text(pos_for_angle(float(ring2_start + ring2_proportion / 2), (2 / 3 + 1) / 2), resp, child, 2, usage2)
+        )
+        hover_response[id] = resp
         ring2_start += ring2_proportion
 
+out.extend(texts)
+
 out.append("<style>")
+out.append(""".infobox { text-anchor: middle; dominant-baseline: middle; font-size: 20pt;
+           fill: black; stroke: white; stroke-width:4pt; paint-order: stroke;
+           font-family: sans-serif; }
+.name {font-weight: bold;}""")
 for source, target in hover_response.items():
     out.append(f"""#{target} {{ display: none;}}""")
-    out.append(f"""#{source}:hover ~ #{target}, #{target}:hover {{display:block;}}""")
+    out.append(f"""#{source}:hover ~ #{target}, #{target}:hover #{source}:active ~ #{target}, #{target}:active
+               {{display:block;}}""")
 out.append("</style>")
 
+out.append(footer)
 with open(sys.argv[2], "wt") as f:
     f.write("\n".join(out))
