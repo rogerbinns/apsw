6e20e4cc92e44c2ff97524be5e145f3e036afd17
Roger Binns <rogerb@rogerbinns.com>
2025-02-25 16:41:44 -0800

    More fleshing out example
    There is some weird issue where bob changeset contains
    stuff from before bob connection was made still being
    investigated.

--- examples/session.py
+++ examples/session.py
@@ -34,13 +34,15 @@ connection.execute(pathlib.Path("session.sql").read_text())
 
 ### adding_session: Monitoring changes
 # You can have multiple :class:`Session` monitoring changes on a
-# session.  You need to call :meth:`Session.attach` to say which tables
-# to monitor, or use :meth:`Session.table_filter` to get a callback.
-# You can pause and resume monitoring with :attr:`Session.enabled`.
+# session.  You need to call :meth:`Session.attach` to say which
+# tables to monitor, or use :meth:`Session.table_filter` to get a
+# callback.  You can pause and resume monitoring with
+# :attr:`Session.enabled`.  For unmonitored changes you can use
+# :meth:`Session.diff` to work out the changes between two tables.
 
 session = apsw.Session(connection, "main")
-# we now say which tables to monitor - None means all
-session.attach(None)
+# we now say which tables to monitor - we want all
+session.attach()
 
 # And now make some changes.  We do every kind of change here -
 # INSERT, UPDATE, and DELETE.
@@ -132,7 +134,7 @@ try:
     apsw.Changeset.apply(undo, connection)
 except apsw.AbortError as exc:
     # It will fail, and the database back in the state when we started
-    # the apply
+    # the apply.
     print(f"Exception {exc=}")
 
 # The reason it failed is because of the foreign keys automatically
@@ -141,6 +143,7 @@ except apsw.AbortError as exc:
 # inspection.
 
 failed = apsw.ChangesetBuilder()
+
 
 # A conflict handler says what to do
 def conflict_handler(reason: int, change: apsw.TableChange) -> int:
@@ -176,7 +179,10 @@ print(
 
 ### syncing: Synchronising changes made by two users
 # Alice and Bob are going to separately work on the same database and
-# we are going to synchronise their changes.
+# we are going to synchronise their changes.  Our database is a bit
+# more complicated because adding to the items table has a trigger
+# that links the ``new`` tag to that item.  We use
+# :meth:`apsw.ext.XXX` which handles that sort of thing.
 
 # Start from the same database
 alice_connection = apsw.Connection("alice.db")
@@ -187,19 +193,40 @@ bob_connection = apsw.Connection("bob.db")
 with bob_connection.backup("main", connection, "main") as backup:
     backup.step()
 
+connection.close()
+
 # setup sessions
 alice_session = apsw.Session(alice_connection, "main")
+alice_session.attach()
 
 bob_session = apsw.Session(bob_connection, "main")
+bob_session.attach()
 
 # Each makes changes
 alice_connection.execute("""
-
-                         """)
+    UPDATE tags SET label='painting' WHERE label='paint';
+    INSERT INTO items(name, description) VALUES('storage closet', 'main storage space');
+    INSERT INTO tags(label) VALUES('approval needed');
+    -- remove new tag
+    DELETE FROM item_tag_link WHERE item_id=(SELECT id FROM items WHERE name='storage closet');
+    -- add approval needed
+    INSERT INTO item_tag_link(item_id, tag_id)  VALUES(
+        (SELECT id FROM items WHERE name='storage closet'),
+        (SELECT id FROM tags WHERE label='approval needed')
+    );
+""")
 
 bob_connection.execute("""
-
-                       """)
+    UPDATE tags SET cost_centre = '150' WHERE label='electrical';
+    INSERT INTO items(name) VALUES('storage B');
+    -- remove new tag
+    DELETE FROM item_tag_link WHERE item_id=(SELECT id FROM items WHERE name='storage B');
+    -- add electrical
+    INSERT INTO item_tag_link(item_id, tag_id)  VALUES(
+        (SELECT id FROM items WHERE name='storage B'),
+        (SELECT id FROM tags WHERE label='electrical')
+    );
+""")
 
 # Get the changesets
 alice_changeset = alice_session.changeset()
@@ -211,6 +238,44 @@ apsw.Changeset.apply(alice_changeset, bob_connection)
 apsw.Changeset.apply(bob_changeset, alice_connection)
 
 
+query = """
+SELECT items.name AS name, tags.label AS tag, tags.cost_centre AS cost_centre
+   FROM tags, items, item_tag_link
+   WHERE items.id = item_tag_link.item_id AND tags.id = item_tag_link.tag_id
+   ORDER BY items.name, tags.label
+"""
+
+print("Alice's database")
+print(apsw.ext.format_query_table(alice_connection, query))
+
+print("\nBob's database")
+print(apsw.ext.format_query_table(bob_connection, query))
+
+print("\nAlice changeset")
+print(
+    "\n".join(
+        apsw.ext.changeset_to_sql(
+            alice_changeset,
+            functools.partial(
+                apsw.ext.find_columns, connection=alice_connection
+            ),
+        )
+    )
+)
+
+print("\nBob changeset")
+print(
+    "\n".join(
+        apsw.ext.changeset_to_sql(
+            bob_changeset,
+            functools.partial(
+                apsw.ext.find_columns, connection=bob_connection
+            ),
+        )
+    )
+)
+
+
 ### builder: ChangesetBuilder
 # The :class:`ChangesetBuilder` can be used to combine multiple
 # changesets and individual :class:`TableChange`.  In this example
@@ -218,9 +283,10 @@ apsw.Changeset.apply(bob_changeset, alice_connection)
 
 items = apsw.ChangesetBuilder()
 
-for change in apsw.Changeset.iter(changeset):
-    if change.name == "items":
-        items.add_change(change)
+for source in (alice_changeset, bob_changeset):
+    for change in apsw.Changeset.iter(source):
+        if change.name == "items":
+            items.add_change(change)
 
 only_items = items.output()
 
@@ -229,7 +295,7 @@ print(
         apsw.ext.changeset_to_sql(
             only_items,
             functools.partial(
-                apsw.ext.find_columns, connection=connection
+                apsw.ext.find_columns, connection=alice_connection
             ),
         )
     )
