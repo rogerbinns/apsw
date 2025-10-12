1c466d3931d3a388494efdffd57e9c90bae71e2b
Roger Binns <rogerb@rogerbinns.com>
2025-10-11 17:12:33 -0700

    updates to deal with new gcc/gcov duplicating lines

--- tools/coverageanalyser.py
+++ tools/coverageanalyser.py
@@ -12,7 +12,7 @@ import os
 def output(filename: str, executed: int, total: int) -> None:
     # Python bug "% 3.2f" doesn't behave correctly (100.00 is formatted with leading space!)
     percent = 100 * executed / max(total, 1)
-    op = [f"{filename:40}", f"{executed:6,}", "  /", f"{total:6,}", "\t    ", "% 3.2f%%" % (percent,)]
+    op = [f"{filename:40}", f"{executed:6,}", "  /", f"{total:6,}", "\t    ", "% 4.2f%%" % (percent,)]
     if percent == 100:
         op[-1] = "100.00%"
     print("".join(op))
@@ -41,15 +41,21 @@ for f in names:
     p = priority(f)
     if p == 4 and lastp == 3:
         lines_executed = lines_total = 0
-    file_exec = 0
-    file_total = 0
+    file_exec = set()
+    file_total = set()
 
     in_test_fixture = False
     with open(f, "rt") as fd:
         for line in fd:
             if ":" not in line:
                 continue
-            count, linenum, line = line.split(":", 2)
+            try:
+                count, linenum, line = line.split(":", 2)
+            except ValueError:
+                # gcc/gcov have been putting duplicate copies of
+                # functions in the output with the function name
+                # and a colon on a line by themselves
+                continue
             line = line.strip()
             if in_test_fixture:
                 if line.startswith("#else") or line.startswith("#endif"):
@@ -61,15 +67,16 @@ for f in names:
             count = count.strip()
             if count == "-":
                 continue
+            linenum = int(linenum)
             if count != "#####":
-                lines_executed += 1
-                file_exec += 1
-            lines_total += 1
-            file_total += 1
+                file_exec.add(linenum)
+            file_total.add(linenum)
     n = os.path.splitext(f)[0]
     if lastp == 3:
         print("-" * 30 + "\n")
-    output(n, file_exec, file_total)
+    output(n, len(file_exec), len(file_total))
+    lines_executed += len(file_exec)
+    lines_total = len(file_total)
     if p < 4:
         print()
     lastp = p
