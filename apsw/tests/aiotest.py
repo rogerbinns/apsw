5fd91bebc8aa415e0e6f187705abb834544d868d
Roger Binns <rogerb@rogerbinns.com>
2025-12-11 10:46:06 -0800

    Cursor iteration corner cases

--- apsw/tests/aiotest.py
+++ apsw/tests/aiotest.py
@@ -4,6 +4,7 @@
 # mypy: ignore-errors
 # type: ignore
 
+import contextlib
 import contextvars
 import unittest
 
@@ -13,16 +14,13 @@ import apsw.aio
 
 #### ::TODO::  tests to add
 #
-# close/aclose closes things and future close calls return None, not an exception
-#
-# prefetch exception sequencing
-#
 # cancels
 #
-# anext call when previous one hasn't finished
-#
 # inheritance of connection/cursor
-
+#
+# blob needs aenter / aexit
+# backup: aclose
+# cursor get/fetchall need to close the cursor (or maybe not?)
 
 
 class Async(unittest.TestCase):
@@ -78,13 +76,74 @@ class Async(unittest.TestCase):
             await (await db.execute("select sync_cvar('inside')")).get
 
     async def atestClosing(self, fw):
-        "check aclose can be called multiple times"
+        "check aclose can be called multiple times, even after object is closed"
         db = await apsw.Connection.as_async(":memory:")
+
+        cursor = await db.execute("select 3")
+
+        await cursor.aclose()
+        await cursor.aclose()
+        cursor.close()
+        cursor.close()
 
         await db.aclose()
         await db.aclose()
         db.close()
+        db.close()
 
+        for obj in cursor, db:
+            await obj.aclose()
+            obj.close()
+
+    async def atestIteration(self, fw):
+        "cursor iteration corner cases"
+        db = await apsw.Connection.as_async(":memory:")
+        await db.execute("create table x(y)")
+        async with db:
+            await db.executemany("insert into x values(?)", ((x,) for x in range(450)))
+
+        def error_at(value, error):
+            if value == error:
+                1 / 0
+            return value
+
+        await db.create_scalar_function("error_at", error_at)
+
+        cur = await db.execute("select * from x")
+        # do anext without aiter call first
+        self.assertRaisesRegex(RuntimeError, "__anext__  called without calling __aiter__", cur.__anext__)
+
+        # check exception sequencing at different batch sizes
+        for prefetch in range(1, 50):
+            for error in range(0, 500):
+                apsw.async_cursor_prefetch.set(prefetch)
+                values = []
+                try:
+                    async for (y,) in await db.execute("select error_at(y, ?) from x order by y", (error,)):
+                        values.append(y)
+                except ZeroDivisionError:
+                    pass
+                self.assertEqual(values, list(range(min(450, error))))
+
+        # check sequential anext without waiting for previous to complete still
+        # give correct answer
+        for prefetch in range(1, 50):
+            for error in range(0, 500):
+                apsw.async_cursor_prefetch.set(prefetch)
+                values = []
+
+                try:
+                    the_iter = aiter(await db.execute("select error_at(y, ?) from x order by y", (error,)))
+                    futures = []
+                    with BatchSends(db):
+                        for i in range(500):
+                            futures.append(anext(the_iter))
+                    for f in futures:
+                        values.append(await f)
+                except ZeroDivisionError:
+                    pass
+
+                self.assertEqual(values, list(range(min(450, error))))
 
     def get_all_atests(self):
         for n in dir(self):
@@ -145,6 +204,29 @@ def sync_get_cvar(name):
 
 async def async_get_cvar(name):
     return sync_get_cvar(name)
+
+
+@contextlib.contextmanager
+def BatchSends(conn):
+    # we swizzle the queue out of the way, grab all the items and then
+    # submit them all at the end
+    class Batcher:
+        def __init__(self):
+            self.batch = []
+
+        def put(self, item):
+            self.batch.append(item)
+
+    batcher = Batcher()
+    actual_queue = conn.async_controller.queue
+    conn.async_controller.queue = batcher
+    try:
+        yield
+    finally:
+        conn.async_controller.queue = actual_queue
+        for item in batcher:
+            actual_queue.put(item)
+
 
 __all__ = ("Async",)
 
