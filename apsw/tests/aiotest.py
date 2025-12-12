ffb8f1747520521ded2b3f897811b7a844afc27f
Roger Binns <rogerb@rogerbinns.com>
2025-12-12 07:01:57 -0800

    Check all the iteration things
    Code passes

--- apsw/tests/aiotest.py
+++ apsw/tests/aiotest.py
@@ -97,10 +97,16 @@ class Async(unittest.TestCase):
 
     async def atestIteration(self, fw):
         "cursor iteration corner cases"
+
+        # values we check
+        NUM_ENTRIES = 450
+        PREFETCH_VALUES = (1, 2, 3, 5, 7, 13, 33, 97, 214, 448, 449, 450, 451, 452, 1024, 2048)
+        LIMITS = (0, 1, 2, 3, 4, 6, 7, 8, 32, 33, 34, 100, 448, 449, 450, 451, 452)
+
         db = await apsw.Connection.as_async(":memory:")
-        await db.execute("create table x(y)")
+        await db.execute("create table x(y INTEGER PRIMARY KEY)")
         async with db:
-            await db.executemany("insert into x values(?)", ((x,) for x in range(450)))
+            await db.executemany("insert into x values(?)", ((x,) for x in range(NUM_ENTRIES)))
 
         def error_at(value, error):
             if value == error:
@@ -113,9 +119,19 @@ class Async(unittest.TestCase):
         # do anext without aiter call first
         self.assertRaisesRegex(RuntimeError, "__anext__  called without calling __aiter__", cur.__anext__)
 
+        # end of query at different batch sizes
+        for prefetch in PREFETCH_VALUES:
+            for limit in LIMITS:
+                apsw.async_cursor_prefetch.set(prefetch)
+                print(f"{prefetch=} {limit=}")
+                values = []
+                async for (y,) in await db.execute("select y from x order by y LIMIT ?", (limit,)):
+                    values.append(y)
+                self.assertEqual(values, list(range(min(NUM_ENTRIES, limit))))
+
         # check exception sequencing at different batch sizes
-        for prefetch in range(1, 50):
-            for error in range(0, 500):
+        for prefetch in PREFETCH_VALUES:
+            for error in LIMITS:
                 apsw.async_cursor_prefetch.set(prefetch)
                 values = []
                 try:
@@ -127,21 +143,28 @@ class Async(unittest.TestCase):
 
         # check sequential anext without waiting for previous to complete still
         # give correct answer
-        for prefetch in range(1, 50):
-            for error in range(0, 500):
+        for prefetch in PREFETCH_VALUES:
+            for error in LIMITS:
                 apsw.async_cursor_prefetch.set(prefetch)
                 values = []
 
                 try:
                     the_iter = aiter(await db.execute("select error_at(y, ?) from x order by y", (error,)))
-                    futures = []
-                    with BatchSends(db):
-                        for i in range(500):
-                            futures.append(anext(the_iter))
-                    for f in futures:
-                        values.append(await f)
                 except ZeroDivisionError:
-                    pass
+                    self.assertEqual(error, 0)
+                    continue
+                futures = []
+                with BatchSends(db):
+                    for i in range(error + 10):
+                        futures.append(anext(the_iter))
+                seen_zerodiv = False
+                for f in futures:
+                    try:
+                        values.append((await f)[0])
+                    except ZeroDivisionError:
+                        seen_zerodiv = True
+                    except StopAsyncIteration:
+                        self.assertTrue(seen_zerodiv or error >= NUM_ENTRIES)
 
                 self.assertEqual(values, list(range(min(450, error))))
 
@@ -224,7 +247,7 @@ def BatchSends(conn):
         yield
     finally:
         conn.async_controller.queue = actual_queue
-        for item in batcher:
+        for item in batcher.batch:
             actual_queue.put(item)
 
 
