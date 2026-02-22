aff2cd6255f1321b2b4d8c03d18df3a637bd8c84
Roger Binns <rogerb@rogerbinns.com>
2026-02-21 17:44:52 -0800

    Initial proof of concept

---
+++ tools/vend.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+
+# Package up all the SQLite extensions and binaries #547
+# most likely exposed as apsw.sqlite_extras
+
+# this code works through building and documenting them first
+# for later refactoring
+
+# doc for source file pattern is https://www.sqlite.org/src/file?ci=trunk&name=ext%2Fmisc%2Frandomjson.c
+
+extras = {
+    "randomjson": {
+        "type": "extension",
+        "sources": ["ext/misc/randomjson.c"],
+        "description": "Generates random json objects",
+    },
+    "sqlite3_rsync": {
+        "type": "executable",
+        "sources": ["tool/sqlite3_rsync.c"],
+        "description": "Database Remote-Copy Tool",
+        "doc": "rsync.html",
+        "libsqlite": True
+    },
+}
+
+import pathlib
+import setuptools._distutils.ccompiler as ccompiler
+from setuptools._distutils.sysconfig import customize_compiler
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format="    %(message)s")
+
+compiler = ccompiler.new_compiler(verbose=True)
+customize_compiler(compiler)
+
+compiler.add_include_dir("sqlite3")
+
+# where the build artifacts go
+build_dir = pathlib.Path() / "build" / "sqlite_extras"
+compiler.mkpath(str(build_dir))
+
+# where the final binaries go
+output_dir = pathlib.Path() / "apsw" / "sqlite_extras_binaries"
+compiler.mkpath(str(output_dir))
+
+# build sqlite3 library
+print(">>> sqlite3 library")
+lib_enables = "CARRAY COLUMN_METADATA DBPAGE_VTAB DBSTAT_VTAB FTS4 FTS5 GEOPOLY MATH_FUNCTIONS PERCENTILE PREUPDATE_HOOK RTREE SESSION".split()
+
+macros = [(f"SQLITE_ENABLE_{enable}", 1) for enable in lib_enables]
+cfg = pathlib.Path("sqlite3") / "sqlite_cfg.h"
+if cfg.exists():
+    macros.append(("SQLITE_CUSTOM_INCLUDE", "sqlite_cfg.h"))
+macros.append(("SQLITE_THREADSAFE", 1))
+lib_objs = compiler.compile([str(pathlib.Path("sqlite3") / "sqlite3.c")], output_dir=str(build_dir), macros=macros)
+
+
+for name, info in extras.items():
+    print(f">>> {name}")
+    missing = []
+    for source in info["sources"]:
+        if not (pathlib.Path("sqlite3") / source).exists():
+            missing.append(source)
+    if missing:
+        print("  Skipping due to missing source:", missing)
+        print()
+        continue
+
+    objs = compiler.compile(
+        [str(pathlib.Path("sqlite3") / filename) for filename in info["sources"]], output_dir=str(build_dir)
+    )
+
+    if info.get("libsqlite", False):
+        objs.extend(lib_objs)
+
+    match info["type"]:
+        case "extension":
+            # ::TODO:: do we want sqlext vs platform native dll extension?
+            out_name = f"{name}.sqlite_extension"
+            compiler.link_shared_object(objs, out_name, output_dir=str(build_dir))
+
+        case "executable":
+            out_name = f"{name}{compiler.exe_extension if compiler.exe_extension else ''}"
+            match compiler.compiler_type:
+                case "msvc":
+                    libraries = None
+                case _:
+                    libraries = ["m"]
+            compiler.link_executable(objs, out_name, output_dir=str(build_dir), libraries=libraries)
+
+        case _:
+            raise NotImplementedError
+
+    compiler.move_file(str(build_dir / out_name), str(output_dir))
+
+    print()
