f91978847bea02a6e0eae20764ed07d8f69be3f9
Roger Binns <rogerb@rogerbinns.com>
2026-09-17 17:13:39 -0700

    PyLong_AsInt overflow
    #624 C103
    
    The next loop iteration would detect a pending
    exception which is why fault injection did not
    discover this.
    
    Also prevent overwriting the OverflowError in a
    later code path

--- src/vtable.c
+++ src/vtable.c
@@ -1535,6 +1535,8 @@ apswvtabBestIndex(sqlite3_vtab *pVtab, sqlite3_index_info *indexinfo)
       if (omitv == -1)
         goto constraintfail;
       indexinfo->aConstraintUsage[i].argvIndex = PyLong_AsInt(argvindex) + 1;
+      if (PyErr_Occurred())
+        goto constraintfail;
       indexinfo->aConstraintUsage[i].omit = omitv;
       Py_DECREF(constraint);
       Py_DECREF(argvindex);
@@ -2039,10 +2041,11 @@ apswvtabFindFunction(sqlite3_vtab *pVtab, int nArg, const char *zName,
       sqliteres = PyLong_AsInt(item_0);
       if (PyErr_Occurred() || sqliteres < SQLITE_INDEX_CONSTRAINT_FUNCTION || sqliteres > 255)
       {
-        PyErr_Format(PyExc_ValueError,
-                     "Expected FindFunction sequence [int, Callable] to have int between "
-                     "SQLITE_INDEX_CONSTRAINT_FUNCTION and 255, not %i",
-                     sqliteres);
+        if (!PyErr_Occurred())
+          PyErr_Format(PyExc_ValueError,
+                       "Expected FindFunction sequence [int, Callable] to have int between "
+                       "SQLITE_INDEX_CONSTRAINT_FUNCTION and 255, not %i",
+                       sqliteres);
         sqliteres = 0;
         goto finally;
       }
